5d7bcea152e94427a043f24ca2376938f1a71fae
kcala <kamil.cala@gmail.com>
2024-12-24 14:40:55 +0100

    solve day1/part2

---
+++ day1/part2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"advent-of-code-2024-go/file"
+	"fmt"
+)
+
+func main() {
+	lines := file.MustReadInput()
+	left, right := parse(lines)
+	solution := solve(left, right)
+	fmt.Println(solution)
+}
+
+func parse(lines []string) (left []int, right []int) {
+	for _, line := range lines {
+		//extract 2 numbers from each line, separated by n spaces
+		var l, r int
+		fmt.Sscanf(line, "%d %d", &l, &r)
+		left = append(left, l)
+		right = append(right, r)
+	}
+	return left, right
+}
+
+func solve(left []int, right []int) int {
+
+	rightCounts := make(map[int]int, 0)
+	for _, r := range right {
+		rightCounts[r] += 1
+	}
+
+	score := 0
+	for _, l := range left {
+		score += l * rightCounts[l]
+	}
+
+	return score
+}
