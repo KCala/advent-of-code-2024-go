6aa50c2ccfb8ef0120cb99fbc985fb13069f5c3f
kcala <kamil.cala@gmail.com>
2025-01-11 16:46:45 +0100

    create day template

---
+++ template/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"advent-of-code-2024-go/file"
+	"fmt"
+)
+
+func main() {
+	input := file.MustReadInput()
+	muls := parse(input)
+	solution := solve(muls)
+	fmt.Println(solution)
+}
+
+func parse(input string) int {
+	return 0
+}
+
+func solve(input int) int {
+	return 0
+}
+
